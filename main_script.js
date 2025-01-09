f37ffd1aa0c987d1927b90f53c4f734d6629a2f7
Quentin <150766542+daguy5134@users.noreply.github.com>
2025-01-09 14:19:32 +0800

    create construction of page based on data from arrays
     - create an iteration over each reminder to build webpage

--- main_script.js
+++ main_script.js
@@ -1,4 +1,4 @@
-class reminder {
+class myReminderClass {
 	constructor (content, name, subject, tag) {
 		this.content = content;
 		this.name = name;
@@ -14,3 +14,34 @@ class tag {
 }
 reminderArr = [];
 tagArr = [];
+
+reminderArr.forEach(function(element, index) {
+	let reminder = document.createElement("div");
+	reminder.class = "reminder";
+	reminder.id = `remDiv${index}`;
+	document.body.appendChild(reminder);
+	const elem_attributes = {
+		title: {
+			htmlType: "h3",
+			elemContent: element.name
+		},
+		topic: {
+			htmlType: "h4",
+			elemContent: element.subject
+		},
+		tagged: {
+			htmlType: "a",
+			elemContent: element.tag
+		},
+		text: {
+			htmlType: "p",
+			elemContent: element.content
+		}
+	}; 
+	Object.keys(myDict).forEach(function(element){
+		let elem = document.createElement(elem_attributes.element.htmlType);
+		elem.class = "reminder";
+		elem.id = `rem${elem_attributes.element.htmlType}${index}`;
+		document.getElementById(`remDiv${index}`).appendChild(elem);
+	});
+});
