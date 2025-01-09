c1feee88c960eff33c2a5810c4f08cafade16831
Quentin <150766542+daguy5134@users.noreply.github.com>
2025-01-09 12:02:00 +0800

    complete classes

--- main_script.js
+++ main_script.js
@@ -1,3 +1,16 @@
 class reminder {
-	constructor (content, )
+	constructor (content, name, subject, tag) {
+		this.content = content;
+		this.name = name;
+		this.subject = subject;
+		this.tag = tag;
+	}
 }
+class tag {
+	constructor (content, bgColor) {
+		this.content = content;
+		this.bgColor = bgColor;
+	}
+}
+reminderArr = [];
+tagArr = [];
