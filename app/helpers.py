30ee62f0acd1500c41c66f99a0c083c8013d37da
daguy5134 <quentin16666@g.lfis.edu.hk>
2025-01-18 12:18:23 +0000

    Update signature verification algo

--- app/helpers.py
+++ app/helpers.py
@@ -4,7 +4,7 @@
 
 import hashlib
 import hmac
-from http.client import HTTPException
+from werkzeug.exceptions import Forbidden
 
 # Ref: https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries
 def verify_signature(payload_body, secret_token, signature_header):
@@ -18,8 +18,10 @@ def verify_signature(payload_body, secret_token, signature_header):
         signature_header: header received from GitHub (x-hub-signature-256)
     """
     if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
-    expected_signature = "sha256=" + hash_object.hexdigest()
-    if not hmac.compare_digest(expected_signature, signature_header):
-        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+        raise Forbidden("x-hub-signature-256 header is missing!")
+    hash_algorithm, github_signature = signature_header.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(secret_token, 'latin-1')
+    mac = hmac.new(encoded_key, msg=payload_body, digestmod=algorithm)
+    if not mac:
+        raise Forbidden("Request signatures didn't match!")
