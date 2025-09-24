4987f4927218d01267962f0568596028ec56cb4d
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-09-24 11:47:08 +0000

    Fix small error with reminder-container

--- frontend/src/components/Reminder.jsx
+++ frontend/src/components/Reminder.jsx
@@ -4,7 +4,7 @@ import adjustColorBrightness from "../modules/adjustColor.js";
 export default function Reminder(props){
     return (
     <div style={styles.container} id={`container${props.id}`}>
-        <div style={Object.assign({}, styles.reminder-container, {backgroundColor: props.subjectColor})} id={`remDiv${props.id}`}>
+        <div style={Object.assign({}, styles.reminder_container, {backgroundColor: props.subjectColor})} id={`remDiv${props.id}`}>
             <a style={Object.assign({}, styles.remindera, {backgroundColor: props.tagColor})} id={`rema${props.id}`}>{props.tag}</a>
             <nobr style={styles.remindernobr} id={`remnobr${props.id}`}>Pour : {props.date}</nobr>
             <h2 style={Object.assign({}, styles.reminderh2, {backgroundColor: adjustColorBrightness(props.tagColor, 70)})} id={`remh2${props.id}`}>{props.subject}</h2>adjustColorBrightness
